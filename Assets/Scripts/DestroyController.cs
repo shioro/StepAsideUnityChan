2aaf9a22dd1d3b49eea0b377635c634f70e77079
shioro <shioro50729@gmail.com>
2021-04-03 21:01:06 +0900

    Initial commit

---
+++ Assets/Scripts/DestroyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyController : MonoBehaviour
+{
+    public GameObject unityChan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //シーン中のunitychanオブジェクトを取得
+        this.unityChan = GameObject.Find("unitychan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dis = unityChan.transform.position.z - gameObject.transform.position.z;
+        Debug.Log("dis:" + dis);
+
+        if(dis > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
